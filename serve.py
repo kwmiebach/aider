ff4b3eaaa5ce523c5aacf6735b6b824fa7e52e8b
kwmiebach <kwmiebach@users.noreply.github.com>
2023-08-21 19:36:33 +0200

    Update serve.py
    Add documentation

--- serve.py
+++ serve.py
@@ -28,6 +28,21 @@ async def ping():
 
 @app.post("/run")
 async def run_command(request: fastapi.Request):
+    """
+    This post endpoint expects one json parameter `command`,
+    which is then executed as  shell command.
+    It makes sense to use the `--message` option with aider to
+    disable chat mode. Example for the content of `command`
+    
+    aider -m \"Write a hello world program in python\"
+
+    The advantage is that the rest service does not depend so much on the
+    internal aider code structure, 
+
+    A disadvantage is that this is very unsecure, so additional measures like
+    https, authentication and maybe restricting or parsing the command
+    is absolutely necessary. See laso the TODO above.
+    """
 
     request_json_data = await request.json()
     command = request_json_data.get("command",None)
